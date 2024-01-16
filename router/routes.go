a3d19fb53b66573e772140f2fe8ed2c50186e586
edubarr <eduaraujobarros@gmail.com>
2024-01-16 16:16:32 -0300

    feat: add swagger for docs

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	"github.com/edubarr/gojobs/docs"
 	"github.com/edubarr/gojobs/handler"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/openings", handler.ListOpeningsHandler)
 		v1.GET("/opening", handler.GetOpeningHandler)
@@ -15,4 +20,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 	}
+
+	// Initialize swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
