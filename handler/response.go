cc905d5a2cd7dc21949a9215cccef7b3798b8655
edubarr <eduaraujobarros@gmail.com>
2024-01-16 15:15:00 -0300

    feat: add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{
+		"errorCode": code,
+		"message":   msg,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"data":    data,
+		"message": fmt.Sprintf("operation %s successful", op),
+	})
+}
