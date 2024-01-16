13136184209cde809d0a9c57c3a458694009a7b3
edubarr <eduaraujobarros@gmail.com>
2024-01-16 17:07:59 -0300

    chore: Added dockerfile and docker-compose for deploy

--- router/router.go
+++ router/router.go
@@ -1,14 +1,23 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func Initialize() {
 	r := gin.Default()
 
 	initializeRoutes(r)
 
-	err := r.Run()
+	// Get the port from the environment
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := r.Run("0.0.0.0:" + port)
 	if err != nil {
 		return
-	} // listen and serve on 0.0.0.0:8080
+	} // listen and serve on 0.0.0.0:port
 }
