e7f8d5ca0b0f748c7e8d509b3cc6e058896803bd
edubarr <eduaraujobarros@gmail.com>
2024-01-16 15:28:57 -0300

    feat: add listOpenings handler

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -1,12 +1,18 @@
 package handler
 
 import (
+	"github.com/edubarr/gojobs/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(context, "list-openings", openings)
 }
