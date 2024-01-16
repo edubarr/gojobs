ec7eb26cf3474ad2c6273d5d2d00eb3cd8b2772f
edubarr <eduaraujobarros@gmail.com>
2024-01-16 15:15:23 -0300

    feat: add createOpening handler

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,40 @@
 package handler
 
 import (
+	"github.com/edubarr/gojobs/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	if err := context.BindJSON(&request); err != nil {
+		sendError(context, http.StatusBadRequest, err.Error())
+		logger.Error("JSON binding error")
+		return
+	}
+
+	if err := request.Validate(); err != nil {
+		sendError(context, http.StatusBadRequest, err.Error())
+		logger.Errorf("validation error: %v", err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, err.Error())
+		logger.Errorf("error creating opening: %v", err.Error())
+		return
+	}
+
+	sendSuccess(context, "create-opening", opening)
 }
